b71479ffce4bfa5ac8c5bcf3bc5889f9a08d874c
z.elqarch <zakariaelqarch@gmail.com>
2024-01-30 11:36:39 +0100

    mongoDb connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-
+import connectDB from './mongodb/connect.js';
 dotenv.config();
 
 const app = express();
@@ -13,7 +13,13 @@ app.get('/', async (req, res) => {
 });
 
 const startServer = async () => {
-    app.listen(8080,()=>console.log("Server has started on port http://localhost:8080"));
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log("Server has started on port http://localhost:8080"));
+    } catch (error) {
+        console.log("MongoDB Error",error)
+    }
 }
 
 startServer();
