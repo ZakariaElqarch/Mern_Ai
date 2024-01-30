ff8a586f253e320fc879db6d0ea5a39cfdc20709
z.elqarch <zakariaelqarch@gmail.com>
2024-01-30 14:54:52 +0100

    linked back-end to front-end for create image (need to upgrade open ai plan to use it)

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,11 +21,34 @@ const CreatePost = () => {
   };
 
   const handleSupriseMe = () => {
-    const randomPrompt =  getRandomPrompt(form.prompt);
+    const randomPrompt = getRandomPrompt(form.prompt);
     setForm({ ...form, prompt: randomPrompt });
   };
 
-  const generateImage = () => {};
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({prompt: form.prompt})
+        });
+
+        const data = await response.json();
+
+        setForm({...form,photo:`data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        alert("API: "+error);
+      } finally{
+        setGeneratingImg(false);
+      }
+    }else{
+      alert('Pleas enter a prompt')
+    }
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,10 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { OpenAI } from 'openai';
-
-
-
-
+import OpenAI from "openai";
 
 dotenv.config();
 
@@ -15,27 +11,25 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 
 router.route('/').get((req, res) => {
-    res.send('hello from dalle')
-})
+    res.send('Hello from DALL-E');
+});
 
 router.route('/').post(async (req, res) => {
     try {
-        const { prompt } = rea.body;
-
-        const aiResponse = await openai.createImage({
+        const { prompt } = req.body;
+        const aiResponse = await openai.images.generate({ 
+            model: 'dall-e-3',
             prompt,
-            n: 1,
-            size: '1024x1024',
-            response_format: 'b64_json'
-
-        })
+            n:1,
+            size:"1024x1024"
+        });
 
         const image = aiResponse.data.data[0].b64_json;
-        res.status(200).json({ photo: image })
+        res.status(200).json({ photo: image });
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error?.response.data.error.message)
+        console.log('API Error: ', error);
+        res.status(500).send(error?.response.data.error.message);
     }
-})
+});
 
-export default router
+export default router;
